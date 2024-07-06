b75756056567fe3df00b204f31486484ec47de2a
jubayer1418 <skjubayer1418@gmail.com>
2024-07-06 18:47:49 +0600

    done

--- src/pages/UpdateProject.jsx
+++ src/pages/UpdateProject.jsx
@@ -50,7 +50,7 @@ const UpdateProject = () => {
   useEffect(() => {
     const getProject = async () => {
       await axios
-        .get(`http://localhost:5000/api/v1/project/get/${id}`, {
+        .get(`https://portfolio-backend-neon-eight.vercel.app/api/v1/project/get/${id}`, {
           withCredentials: true,
         })
         .then((res) => {
